94f8ac44cfdf5a60196867eb9151f2ec64955d49
muhrmps123@gmail.com <muhrmps123@gmail.com>
2022-12-21 00:58:32 +0700

    feat: update add,edit and index

--- app.js
+++ app.js
@@ -12,66 +12,66 @@ const server = OrientDB({
 });
 
 const db = server.use({
-    name: 'databook',
+    name: 'datamahasiswa',
     username: 'root',
     password: 'rootpwd',
 });
 
 
-db.exec('CREATE CLASS Book IF NOT EXISTS EXTENDS V');
-
-db.exec('CREATE PROPERTY Book.id IF NOT EXISTS INTEGER (MANDATORY TRUE)');
-db.exec('CREATE PROPERTY Book.title IF NOT EXISTS STRING');
-db.exec('CREATE PROPERTY Book.author IF NOT EXISTS STRING');
-db.exec('CREATE PROPERTY Book.year IF NOT EXISTS STRING');
+db.exec('CREATE CLASS Mahasiswa IF NOT EXISTS EXTENDS V');
+db.exec('CREATE PROPERTY Mahasiswa.id IF NOT EXISTS INTEGER (MANDATORY TRUE)');
+db.exec('CREATE PROPERTY Mahasiswa.name IF NOT EXISTS STRING');
+db.exec('CREATE PROPERTY Mahasiswa.jurusan IF NOT EXISTS STRING');
+db.exec('CREATE PROPERTY Mahasiswa.angkatan IF NOT EXISTS INTEGER');
+db.exec('CREATE PROPERTY Mahasiswa.dospem IF NOT EXISTS STRING');
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/',async (req, res) => {
-    const sql = 'SELECT FROM Book';
+    const sql = 'SELECT FROM Mahasiswa';
     const result = await db.query(sql);
-    res.render('index', { books: result });
+    res.render('index', { mahasiswas: result });
 });
 
 app.get('/add', (req, res) => {
-    res.render('add-book');
+    res.render('add-Mahasiswa');
 });
 
 app.post('/add', async (req, res) => {
-    const { title, author, year } = req.body;
-    const sql = 'INSERT INTO Book (id, title, author, year) VALUES (:id, :title, :author, :year)';
-    const idquery = 'SELECT MAX(id) FROM Book';
+    const { name, jurusan, angkatan, dospem } = req.body;
+    const sql = 'INSERT INTO Mahasiswa (id, name, jurusan, angkatan, dospem) VALUES (:id, :name, :jurusan, :angkatan, :dospem)';
+    const idquery = 'SELECT MAX(id) FROM Mahasiswa';
     const id = await db.query(idquery).then((result) => {
         if (result[0]['MAX'] == null) {
             return 1;
         }
         return result[0]['MAX'] + 1;
     });
-    const result = await db.query(sql, { params: { id, title, author, year } });
+    const result = await db.query(sql, { params: { id, name, jurusan, angkatan, dospem } });
     res.redirect('/');
 });
 
 app.get('/delete/:id', async (req, res) => {
     const { id } = req.params;
-    const sql = 'DELETE VERTEX FROM Book WHERE id = :id';
+    const sql = 'DELETE VERTEX FROM Mahasiswa WHERE id = :id';
     const result = await db.query(sql, { params: { id } });
     res.redirect('/');
 });
 
 app.get('/edit/:id', async (req, res) => {
     const { id } = req.params;
-    const sql = 'SELECT FROM Book WHERE id = :id';
+    const sql = 'SELECT FROM Mahasiswa WHERE id = :id';
     const result = await db.query(sql, { params: { id } });
-    res.render('edit-book', { book: result[0] });
+    res.render('edit-Mahasiswa', { mahasiswa: result[0] });
 });
 
 app.post('/edit/:id', async (req, res) => {
     const { id } = req.params;
-    const { title, author, year } = req.body;
-    const sql = 'UPDATE Book SET title = :title, author = :author, year = :year WHERE id = :id';
-    const result = await db.query(sql, { params: { id, title, author, year } });
+    const { name, jurusan, angkatan, dospem } = req.body;
+    const sql = 'UPDATE Mahasiswa SET name = :name, jurusan = :jurusan, angkatan = :angkatan, dospem = : dospem WHERE id = :id';
+    const result = await db.query(sql, { params: { id, name, jurusan, angkatan, dospem } });
     res.redirect('/');
 });
 
